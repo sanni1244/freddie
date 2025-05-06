2a475c647e7eb00af8377886466c4392fb290ba5
Opeyemi <astro8va@gmail.com>
2025-05-06 19:42:27 +0100

    hkhs

--- src/app/pages/formaction/page.tsx
+++ src/app/pages/formaction/page.tsx
@@ -1,7 +1,22 @@
-import React from 'react'
+'use client';
+import React, { useState, useEffect } from 'react';
+import { Managerdetails } from '@/types';
+import { motion, AnimatePresence } from 'framer-motion';
+import DisplayManagers from '../../components/display/displaymanagers';
+import CreateManager from '../../components/create/createmanager';
+import EditManager from '../../components/edit/editmanager';
+import SuccessMessage from '@/app/components/success';
+import ErrorMessage from '@/app/components/error';
+import api from '@/lib/api';
 
-export const page = () => {
+const Formspage = () => {
+  
   return (
-    <div>page</div>
-  )
-}
+    <div className="p-6">
+      <p>fhgvjbkm</p>
+    </div>
+  );
+};
+
+export default Formspage;
+
